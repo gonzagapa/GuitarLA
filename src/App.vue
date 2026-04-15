cac11403b9d8c0d3f17cc220c6994bc0e13c2733
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2026-04-14 22:01:03 -0600

    feat: define Header component events for Cart

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import {ref,reactive, onMounted} from "vue";
 import {db} from './data/guitarras';
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 import GuitarraCard from "./components/GuitarraCard.vue";
 
 // const state = reactive({
@@ -14,6 +15,15 @@ const guitarras = ref(db);
 const carritos = ref([])
 
 const agregarCarrito = (guitarra)=>{
+
+    let existeGuitarra = carritos.value.find( item => item.id === guitarra.id);
+    if(existeGuitarra){
+
+        existeGuitarra.cantidad += 1;
+        return;
+    }
+
+    guitarra.cantidad = 1;
     carritos.value.push(guitarra)
 }
 
@@ -21,10 +31,21 @@ onMounted(()=>{
     console.log("Componente montado");
 })
 
+const incrementarCantidad = ()=>{
+    console.log("Incrementar....")
+}
+
+const decrementarCantidad = ()=>{
+    console.log("decrementar...")
+}
+
 </script>
 
 <template>
-    <Header/>
+    <Header  
+    @incrementar-cantidad="incrementarCantidad"
+    @decrementar-cantidad="decrementarCantidad"
+    :carritos="carritos"/>
     <!-- MAIN CONTENT -->
       <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
@@ -35,8 +56,7 @@ onMounted(()=>{
 
         </div>
     </main>
+
+    <Footer/>
 </template>
 
-<style scoped>
-
-</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,12 @@
 <script setup >
+    const props = defineProps({
+        carritos:{
+            type:Array,
+            required:true
+        }
+    })
 
+    defineEmits(['incrementar-cantidad', 'decrementar-cantidad'])
 </script>
 
 <template>
@@ -18,55 +25,59 @@
                         <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" class="bg-white p-3">
-                            <p class="text-center">El carrito esta vacio</p>
-                            <table class="w-100 table">
-                                <thead>
-                                    <tr>
-                                        <th>Imagen</th>
-                                        <th>Nombre</th>
-                                        <th>Precio</th>
-                                        <th>Cantidad</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <img class="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra">
-                                        </td>
-                                        <td>SRV</td>
-                                        <td class="fw-bold">
-                                                $299
-                                        </td>
-                                        <td class="flex align-items-start gap-4">
-                                            <button
-                                                type="button"
-                                                class="btn btn-dark"
-                                            >
-                                                -
-                                            </button>
-                                                1
-                                            <button
-                                                type="button"
-                                                class="btn btn-dark"
-                                            >
-                                                +
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button
-                                                class="btn btn-danger"
-                                                type="button"
-                                            >
-                                                X
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
-                            <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
-                            <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                            <p v-if="carritos.length < 1 " class="text-center">El carrito esta vacio</p>
+                            <div v-else>
+                                <table class="w-100 table">
+                                    <thead>
+                                        <tr>
+                                            <th>Imagen</th>
+                                            <th>Nombre</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="elemento in carritos">
+                                            <td>
+                                                <img class="img-fluid" :src="'img/'+ elemento.imagen + '.jpg'" :alt="'imagen guitarra' + elemento.nombre">
+                                            </td>
+                                            <td>{{ elemento.nombre }}</td>
+                                            <td class="fw-bold">
+                                                   {{ elemento.precio }}
+                                            </td>
+                                            <td class="flex align-items-start gap-4">
+                                                <button
+                                                    type="button"
+                                                    class="btn btn-dark"
+                                                    @click="$emit('decrementar-cantidad')"
+                                                >
+                                                    -
+                                                </button>
+                                                    {{ elemento.cantidad }}
+                                                <button
+                                                    type="button"
+                                                    class="btn btn-dark"
+                                                    @click="$emit('incrementar-cantidad')"
+                                                >
+                                                    +
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button
+                                                    class="btn btn-danger"
+                                                    type="button"
+                                                >
+                                                    X
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+    
+                                <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
+                                <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                            </div>
                         </div>
                     </div>
                 </nav>
